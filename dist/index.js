082e8154631791b311de222e8d22365daecfd491
Agmund2002 <malysh.andrey2000@gmail.com>
2023-11-25 17:39:16 +0200

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
-"use strict";
+import { concatenation } from "./concatenation";
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+if (button && input) {
+    button.addEventListener("click", () => {
+        concatenation(input.value, "hello!");
+        input.value = '';
+    });
+}
 //# sourceMappingURL=index.js.map
